6cd07c8e0b982724870cab655a4d5cf796fb5bbe
juhyun4910 <joomong4910@naver.com>
2024-06-28 15:20:51 +0900

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SpeechToTextExample(),
+    );
+  }
+}
+
+class SpeechToTextExample extends StatefulWidget {
+  @override
+  _SpeechToTextExampleState createState() => _SpeechToTextExampleState();
+}
+
+class _SpeechToTextExampleState extends State<SpeechToTextExample> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = "음성 인식을 시작하려면 버튼을 누르세요";
+  double _confidence = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Speech to Text Demo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _listen,
+        child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Text(
+            '$_text\n',
+            style: const TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
